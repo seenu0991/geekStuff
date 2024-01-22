75f8e2f67a797cc925aadd755b282b2529c9ab1e
ROot-Tesla <simbustudies@gmail.com>
2024-01-22 18:10:43 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,22 +1,22 @@
+
 import dns.resolver
 import csv 
 import socket 
 import sys
 import os
 import easygui
-
+import whois
 
 inputfile = easygui.fileopenbox(filetypes=['dns.txt'])
-with open(inputfile, 'r') as ins:
-    with open('ip.txt','a') as out:
-        for dns in ins.readlines():
-            #dns = dns.stirp()
-            try:
-                print('resolving:'+dns)
-                ip = socket.gethostbyname(dns)
-                out.write(dns + '-' + ip + '\n')
-            except:
-                print('unable to resolve:' + dns)
 
-#dm_fin = whois.whois('ballade.real.notaires.com')
-#print(dm_fin)
+with open(inputfile, 'r') as ins:
+    with open('ips.csv', 'w') as out:
+        for host in ins.readlines():
+            try:
+                print('resolving:', host.strip())
+                ip = socket.gethostbyname(host.strip())
+                print('whois')
+                dns = whois.whois(ip)
+                out.write(f"{ip,host} registrar:{dns.registrar} updated_date:{dns.updated_date} created_date:{dns.creation_date} org:{dns.org} country:{dns.country}\n")
+            except Exception as e:
+                out.write(f'unable to resolve: {host.strip()}. Error: {e}')
